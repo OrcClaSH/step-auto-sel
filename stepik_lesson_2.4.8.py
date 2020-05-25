e1a26a67bb32cd7ff25c341ff261a8bf04fbb84e
OrcClaSH <orcaclash@gmail.com>
2020-05-25 13:42:23 +0900

    Добавил перехват/игнорирование всех ошибок и автоматическое завершение работы браузера в любом из случаев.

--- stepik_lesson_2.4.8.py
+++ stepik_lesson_2.4.8.py
@@ -9,17 +9,19 @@ def calc(x):
     return str(math.log(abs(12 * math.sin(int(x)))))
 
 browser = webdriver.Chrome()
-browser.get('http://suninjuly.github.io/explicit_wait2.html')
+try:
+    browser.get('http://suninjuly.github.io/explicit_wait2.html')
 
-price = WebDriverWait(browser, 12).until(
-    EC.text_to_be_present_in_element((By.ID, 'price'), '100')
-)
+    price = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, 'price'), '100')
+    )
 
-browser.find_element(By.ID, 'book').click()
+    browser.find_element(By.ID, 'book').click()
 
-x = browser.find_element(By.ID, 'input_value').text
-browser.find_element(By.ID, 'answer').send_keys(calc(x))
-browser.find_element(By.ID, 'solve').click()
+    x = browser.find_element(By.ID, 'input_value').text
+    browser.find_element(By.ID, 'answer').send_keys(calc(x))
+    browser.find_element(By.ID, 'solve').click()
 
-print('Ответ: ', browser.switch_to.alert.text.split(': ')[-1])
-browser.quit()
+    print('Ответ: ', browser.switch_to.alert.text.split(': ')[-1])
+finally:
+    browser.quit()
